6fce60769e7eb62521f8e7e8268ddb0dca94215e
Patryk Bogusz <b3.bogusz@gmail.com>
2022-08-04 14:54:21 +0200

    add authentication, lazy loading, Signin and Signout pages

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {createMemoryHistory, createBrowserHistory} from 'history';
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+// Mount function to start up the app
+const mount = (el, { onSignIn, onNavigate, defaultHistory, initialPath }) => {
     const history = defaultHistory || createMemoryHistory({
         initialEntries: [initialPath]
     });
@@ -12,7 +13,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
         history.listen(onNavigate);
     }
 
-    ReactDOM.render( <App history={history}/>, el);
+    ReactDOM.render( <App onSignIn={onSignIn} history={history}/>, el);
 
     return {
         onParentNavigate({ pathname: nextPathname }) {
@@ -28,7 +29,7 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 //If we are in development and in isolation,
 //call mount immediately
 if (process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+    const devRoot = document.querySelector('#_auth-dev-root');
 
     if (devRoot) {
         mount(devRoot, { defaultHistory: createMemoryHistory() });
